a0f19ccea9649ef0a084f9fb95325c55d1f88eb8
Shehzad Murtaza <mr.shehzad457@gmail.com>
2024-12-17 16:59:14 +0500

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu"; // Removed ProductItem as it's not used
+import { Menu, MenuItem } from "./ui/navbar-menu"; // Removed ProductItem as it's not used
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
@@ -14,17 +14,11 @@ function Navbar({ className }: { className?: string }) {
         <Link href={"/"}>
           <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
         </Link>
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/courses">All Courses</HoveredLink>
-            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
-            <HoveredLink href="/courses">Songwriting</HoveredLink>
-            <HoveredLink href="/courses">Music Production</HoveredLink>
-          </div>
-        </MenuItem>
-        <Link href={"/contact"}>
-          <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
+        <Link href={"/login"}>
+          <MenuItem setActive={setActive} active={active} item="Login"></MenuItem>
+        </Link>
+        <Link href={"/signup"}>
+          <MenuItem setActive={setActive} active={active} item="Signup"></MenuItem>
         </Link>
       </Menu>
     </div>
